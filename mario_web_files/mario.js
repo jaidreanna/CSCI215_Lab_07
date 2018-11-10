4af3de0a68bbbfb5ec955458f613f88d50270e8d
jaidreanna <jaidreanna@gmail.com>
2018-11-10 00:42:03 -0500

    mario on the ground!

--- mario_web_files/mario.js
+++ mario_web_files/mario.js
@@ -1,4 +1,4 @@
-// JavaScript for creating interactive animations on a canvas 
+// JavaScript for creating interactive animations on a canvas
 ////////////////////////////////////////////////////////////////////
 // Create a Mario object which contains all the info about Mario
 // Objects are nice because they allow up to keep all the relevant
@@ -14,36 +14,41 @@ var bgImage = new Image();
 
 // Is called when the window loads;
 function init() {
-	
-	// Initialize Mario Object
-	// TODO: Put Mario on the ground instead of the cloud
+
+    // Initialize Mario Object
+    // TODO: Put Mario on the ground instead of the cloud
+
+    function printMousePos(event) {
+        document.body.textContent =
+            "clientX: " + event.clientX +
+            " - clientY: " + event.clientY;
+    }
+
+    document.addEventListener("click", printMousePos);
 
 
-
-	Mario = {
-		x: 100,
-		y: 280,
-		w: 50,
-		h: 80,
-		JumpSound: new Audio('jump.wav'),
-		Image: (function() {
-			var temp = new Image();
-			temp.src = "mario1.png";
-			return temp;})(),
-		moving: "no",
-		timer: "",
-		timerInterval: 10
-	};
-
-	bgImage.src = "marioBG.jpg";
-	draw();
-
-    ctx.drawImage(Mario.x, Mario.y, Mario.w, Mario.h);
+	var canvas = document.getElementById("mario_canvas");
 
 
-	// TODO: (OPTIONAL) set mario_08.wav as background music
+    Mario = {
+        x: 172,
+        y: 604,
+        w: 50,
+        h: 80,
+        JumpSound: new Audio('jump.wav'),
+        Image: (function() {
+            var temp = new Image();
+            temp.src = "mario1.png";
+            return temp;})(),
+        moving: "no",
+        timer: "",
+        timerInterval: 10
+    };
 
+    bgImage.src = "marioBG.jpg";
+    draw();
 
+    // TODO: (OPTIONAL) set mario_08.wav as background music
 
 }
 
@@ -51,72 +56,77 @@ function init() {
 
 function draw() {
 
-	// Get Drawing Area
-	var ctx = document.getElementById("mario_canvas").getContext("2d");
-	
-	// If you want to display images on the canvas when it is initially
-	// loaded, you must do it this way
-	bgImage.onload = function(){
-		ctx.drawImage(bgImage, 0, 0);
-    };
+    // Get Drawing Area
+    var ctx = document.getElementById("mario_canvas").getContext("2d");
 
-	/*
-	 * TODO: Draw Mario's initial image
-	 */
-	
+    // If you want to display images on the canvas when it is initially
+    // loaded, you must do it this way
+    bgImage.onload = function(){
+        ctx.drawImage(bgImage, 0, 0);
 
-	/////////////////////////////////////////////////////////////////
-	var render = function () {
-		ctx.drawImage(bgImage, 0, 0); 
-		renderMario();
-	};
+    }
 
-	/*
-	 * TODO: Alter the y coordinates so Mario will jump while on the ground
-	 */
-	function renderMario(){
-		if (Mario.y > 200 && Mario.moving == "up") {
-			Mario.Image.src = "mario2.png";
-			ctx.drawImage(Mario.Image, Mario.x, Mario.y, Mario.w, Mario.h);
-			// Change the y value each time 
-			Mario.y -= 5; // move 5 px up
-		}else if(Mario.y <= 200 && Mario.moving == "up"){
-			Mario.moving = "down";
-		} else if(Mario.y < 280 && Mario.moving == "down"){
-			Mario.Image.src = "mario2.png";
-			ctx.drawImage(Mario.Image, Mario.x, Mario.y, Mario.w, Mario.h);
-			Mario.y += 5; // move 5 px back down after a jump
-		}else if(Mario.y == 280 && Mario.moving == "no"){
-			Mario.moving = "up";
-			Mario.JumpSound.play();
-		}else{
-			Mario.moving = "no";
-			Mario.Image.src = "mario1.png";
-			ctx.drawImage(Mario.Image, Mario.x, Mario.y, Mario.w, Mario.h);
-			clearInterval(Mario.timer); // kills the timer
-		}	
-	}
-	///////////////////////////////////////////////////////////////////
+    /*
+     * TODO: Draw Mario's initial image
+     */
 
 
-	/* Monitor key strokes for user input:
-	 *
-	 * If Enter/Return is pressed, then call the render function
-	 * which paints the new scene to the canvas.
-	 *
-	 * TODO: Add code to set Mario image to proper image whether L or R button pressed
-	 * TODO: Stop Mario if he runs out of room
-	 *
-	 */
-	document.body.onkeydown = function(e) {  // listen for a key
 
-    	e = event || window.event;             // any kind of event
-    	var keycode = e.charCode || e.keyCode; // any kind of key
-		console.log(keycode);
-		// The user wants Mario to jump:
-    	if(keycode === 13 && Mario.moving == "no") {  
-        	Mario.timer = setInterval(render, Mario.timerInterval); 
-    	}
+
+
+
+    /////////////////////////////////////////////////////////////////
+    var render = function () {
+        ctx.drawImage(bgImage, 0, 0);
+        renderMario();
+    }
+
+    /*
+     * TODO: Alter the y coordinates so Mario will jump while on the ground
+     */
+    function renderMario(){
+        if (Mario.y > 200 && Mario.moving == "up") {
+            Mario.Image.src = "mario2.png";
+            ctx.drawImage(Mario.Image, Mario.x, Mario.y, Mario.w, Mario.h);
+            // Change the y value each time
+            Mario.y -= 5; // move 5 px up
+        }else if(Mario.y <= 200 && Mario.moving == "up"){
+            Mario.moving = "down";
+        } else if(Mario.y < 280 && Mario.moving == "down"){
+            Mario.Image.src = "mario2.png";
+            ctx.drawImage(Mario.Image, Mario.x, Mario.y, Mario.w, Mario.h);
+            Mario.y += 5; // move 5 px back down after a jump
+        }else if(Mario.y == 280 && Mario.moving == "no"){
+            Mario.moving = "up";
+            Mario.JumpSound.play();
+        }else{
+            Mario.moving = "no";
+            Mario.Image.src = "mario1.png";
+            ctx.drawImage(Mario.Image, Mario.x, Mario.y, Mario.w, Mario.h);
+            clearInterval(Mario.timer); // kills the timer
+        }
+    }
+    ///////////////////////////////////////////////////////////////////
+
+
+    /* Monitor key strokes for user input:
+     *
+     * If Enter/Return is pressed, then call the render function
+     * which paints the new scene to the canvas.
+     *
+     * TODO: Add code to set Mario image to proper image whether L or R button pressed
+     * TODO: Stop Mario if he runs out of room
+     *
+     */
+    document.body.onkeydown = function(e) {  // listen for a key
+
+        e = event || window.event;             // any kind of event
+        var keycode = e.charCode || e.keyCode; // any kind of key
+        console.log(keycode);
+        // The user wants Mario to jump:
+        if(keycode === 13 && Mario.moving == "no") {
+            Mario.timer = setInterval(render, Mario.timerInterval);
+        }
 
 
 
@@ -137,5 +147,5 @@ function draw() {
     function faceForward() {
 
     }
-	
-} // close draw() 
+
+} // close draw()
